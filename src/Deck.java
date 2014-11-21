c23d86f21d862abbc95519b51cf873305277f111
arteag56 <arteag56@students.rowan.edu>
2014-11-21 09:35:49 -0500

    dfbfd
    dfgbf

--- src/Deck.java
+++ src/Deck.java
@@ -9,65 +9,65 @@ import java.util.*;
  *
  */
 public class Deck {
-	
+
 	public Set<String> temp;
 	public HashMap <Integer, Set <String>>  allCards;
 	public Stack<Integer> deck;
 	public ArrayList<Integer> hand;
 
 	public void allCards(){
-	 try
-     {
-     String line;
-     int key=0;
-     Scanner scan = new Scanner (new File ("MonsterDeckTest.txt"));
-     while(scan.hasNextLine())
-     {key++;       	
-     	for(int i=0;i<4;i++)
-         {   line = scan.nextLine();
-             temp.add(line);
-         }
-     	allCards.put(key, temp);
-     	temp.clear();
-     }
-     scan.close();
-     }
-     catch (FileNotFoundException fnfe)
-     {   System.err.println (fnfe);  }
+		try
+		{
+			String line;
+			int key=0;
+			Scanner scan = new Scanner (new File ("MonsterDeckTest.txt"));
+			while(scan.hasNextLine())
+			{key++;       	
+			for(int i=0;i<4;i++)
+			{   line = scan.nextLine();
+			temp.add(line);
+			}
+			allCards.put(key, temp);
+			temp.clear();
+			}
+			scan.close();
+		}
+		catch (FileNotFoundException fnfe)
+		{   System.err.println (fnfe);  }
 	} 
-	  
-	  
 
 
-	  private int addDeck (int cardNumber)
-	  {return deck.push(cardNumber); 
-	   
-	  }
-	  
-	  public int addHand(int cardNumber)
-	  {hand.add(cardNumber);
-	  return cardNumber;	  
-	  }
-	  
-	  public int next()
-	  {return deck.pop();
-	  }
-	  
-	  public int remove(int cardNumber)
-	  {return hand.remove(cardNumber);
-	  }
-	  
-	  public Set<String> attributes(int key)
-	  {return allCards.get(key);
-	  }
-	  
-	  public void main()
-	  {allCards();
-	  addDeck(1);
-	  int key=next();
-	  System.out.println(attributes(key));
-	  
-		  
-	  }
+
+
+	private int addDeck (int cardNumber)
+	{return deck.push(cardNumber); 
+
+	}
+
+	public int addHand(int cardNumber)
+	{hand.add(cardNumber);
+	return cardNumber;	  
+	}
+
+	public int next()
+	{return deck.pop();
+	}
+
+	public int remove(int cardNumber)
+	{return hand.remove(cardNumber);
+	}
+
+	public Set<String> attributes(int key)
+	{return allCards.get(key);
+	}
+
+	public void main()
+	{allCards();
+	addDeck(1);
+	int key=next();
+	System.out.println(attributes(key));
+
+
+	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -9,78 +9,54 @@ import java.util.*;
  */
 public class Player
 {
-    List<MagicCard> magicDeck = new ArrayList<MagicCard>();
-    List<CreatureCard> monsterDeck = new ArrayList<CreatureCard>();
-    List<Card> currentHand = new ArrayList<Card>();
+	Deck deck = new Deck();
+	List<MagicCard> magicDeck = new ArrayList<MagicCard>();
+	List<CreatureCard> monsterDeck = new ArrayList<CreatureCard>();
+	List<Card> currentHand = new ArrayList<Card>();
 
-    /**
-     * Constructor for objects of class Player
-     */
-    public Player() {
-<<<<<<< Updated upstream
-     
-        try
-        {
-        String line;
-        Scanner scan = new Scanner (new File ("monsterdeck.txt"));
-        while (scan.hasNextLine())
-            {   line = scan.nextLine();
-                ///monsterDeck.add (line);
-            }
-        scan.close();
-        }
-        catch (FileNotFoundException fnfe)
-        {   System.err.println (fnfe);  }
-    }
-    
-    public static void main(String args[]) {
-    	
-    }
-=======
-    	
-    }
-     
-        
->>>>>>> Stashed changes
-
-        /** @return The list of words generated
-         *  by the constructor.
-         */
-<<<<<<< Updated upstream
-    //public List <String> getMonsterDeck() { return monsterDeck; }
-     
-=======
-   public List<CreatureCard> getMonsterDeck() {
-	   return monsterDeck;  
+	/**
+	 * Constructor for objects of class Player
+	 */
+	public Player() {
+		try {
+			String line;
+			Scanner scan = new Scanner (new File ("monsterdeck.txt"));
+			while (scan.hasNextLine()) {   
+				line = scan.nextLine();
+				//monsterDeck.add (line);
+			}
+			scan.close();
+		}
+		catch (FileNotFoundException fnfe)
+		{   System.err.println (fnfe);  }
 	}
-        
-    
-    
-    
->>>>>>> Stashed changes
-    public void addMonsterDeck() {
 
-    }
-    
-    
-    public void addMagicDeck() {
-        //reads input from file
-    }
-    
-    public List<Card> getHand() {
-        return currentHand; 
-    }
-    
-    public List<Card> getDeck() {
-        List<Card> deck = new ArrayList<Card>();
-        for (Card s: monsterDeck) {
-            deck.add(s);
-        }
-        
-        for (Card s: magicDeck) {
-            deck.add(s);
-        }
-        return deck;
-    }
+	public static void main(String args[]) {
+
+	}
+
+	/** @return The list of words generated
+	 *  by the constructor.
+	 */
+
+	public List<CreatureCard> getMonsterDeck() {
+		return monsterDeck;  
+	}
+
+	public List<Card> getHand() {
+		return currentHand; 
+	}
+
+	public List<Card> getDeck() {
+		List<Card> deck = new ArrayList<Card>();
+		for (Card s: monsterDeck) {
+			deck.add(s);
+		}
+
+		for (Card s: magicDeck) {
+			deck.add(s);
+		}
+		return deck;
+	}
 
 }

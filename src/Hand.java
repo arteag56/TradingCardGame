ddcdc744be9e4052f6ffc520140a886c016cbedf
arteaga56 <arteag56@students.rowan.edu>
2014-11-30 04:09:21 -0500

    Hand
    Can now read input and create deck with creatures

--- src/Hand.java
+++ src/Hand.java
@@ -7,107 +7,109 @@ import java.util.*;
  *
  */
 public class Hand {
-	private Scanner scan;
-	//private Map<Hashtable<Integer, Integer>, Card> hand = new TreeMap <Hashtable<Integer, Integer>, Card> ();
-	private Map<String, Card> hand = new TreeMap <String, Card> ();
+	private Map<String, Card> hand = new TreeMap<String, Card>();
+	private Map<String, Card> deck = new TreeMap<String, Card>();
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Hand hand = new Hand();
-		//hand.openFile();
-		hand.addAllCards();
+		hand.createDeck();
 
 	}
-	
+
 	/**
-     * Obtain the name of the file and open the file for reading. Attempt to 
-     * recover from the typical I/O exceptions with a limited number of retries (3).
-     */
-    public static Scanner openFile() {
-        String nameOfFile; 
-        Scanner scan = null;
-        boolean success = false;
-        int attempt = 3;
+	 * Obtain the name of the file and open the file for reading. Attempt to
+	 * recover from the typical I/O exceptions with a limited number of retries
+	 * (3).
+	 */
+	public static Scanner openFile() {
+		String nameOfFile;
+		Scanner scan = null;
+		boolean success = false;
+		int attempt = 3;
 
-        while (attempt > 0 && !success) {
+		while (attempt > 0 && !success) {
 
-            System.out.println("Enter Name of File: ");
-            Scanner inputRead = new Scanner(System.in);
-            nameOfFile = inputRead.next();
+			System.out.println("Enter Name of File: ");
+			Scanner inputRead = new Scanner(System.in);
+			nameOfFile = inputRead.next();
 
-            try{
-                scan = new Scanner(new File(nameOfFile));
-                success = true;
-                System.out.println("\n" + nameOfFile + " was read successfully!");
-            }
-            catch(IOException e){
-                System.out.println(e.getMessage() + "\n");
-                attempt--;
-                if (attempt > 0) {
-                    System.out.println(attempt + " attempts left.");
-                }
-                else {
-                    System.out.println("You failure.");
-                }
+			try {
+				scan = new Scanner(new File(nameOfFile));
+				success = true;
+				System.out.println("\n" + nameOfFile
+						+ " was read successfully!");
+			} catch (IOException e) {
+				System.out.println(e.getMessage() + "\n");
+				attempt--;
+				if (attempt > 0) {
+					System.out.println(attempt + " attempts left.");
+				} else {
+					System.out.println("You failure.");
+				}
 
-            }
+			}
 
-        }//while loop
-        return scan;
-    }
-	
-    public void addAll() {
-    	Scanner scan = openFile();
-    	ArrayList<String> list = new ArrayList<String>();
-    	while (scan.hasNextLine()) {
-    		list.add(scan.nextLine());
-    		
-    		list.remove(0);
-    	}
-    	System.out.println(list);
-    	
-    }
-    
-	public Map<Hashtable<Integer, Integer>, Card> viewHand() {
-		return null;
+		}// while loop
+		return scan;
 	}
 	
-	public void addCard(Card c) {
+	public void createDeck() {
+		ArrayList<String> list = new ArrayList<String>();
+		Scanner scan = openFile();
+		scan.useDelimiter(",");
+		while (scan.hasNext()) {
+			list.add(scan.next());
+			if (list.size() == 7) {
+				boolean rare = false;
+				if (list.get(6).trim().toLowerCase().equals("true")) {
+					rare = true;
+				}
+				CreatureCard c = new CreatureCard(
+						Integer.parseInt(list.get(1).trim()), list.get(2),
+						Integer.parseInt(list.get(3).trim()), Integer.parseInt(list.get(4).trim()), 
+						list.get(5).trim(), rare);
+				deck.put(list.get(0).trim() + c.getCardNum(), c);
+				list.clear();
+			}
+		}
+		//System.out.println(list); Sanity check
+		System.out.println(deck);
+
+	}
+	
+	public boolean addCardToDeck(String type, Card c) {
+		if (type.equals("C") || type.equals("C")) {
+			deck.put(type + c.getCardNum(), c);
+			return true;
+		}
+		else if (type.equals("M") || type.equals("m")) {
+			deck.put(type + c.getCardNum(), c);
+			return true;
+		}
+		else {
+			return false;
+		}
 		
 	}
-	public void addAllCards(){
-        ArrayList<String> list = new ArrayList<String>();
-        Scanner scan = openFile();
-        //scan.useDelimiter(",");
-        
-        while (scan.hasNextLine()) {
-            Scanner scanny = scan;
-            //System.out.println(scan.nextLine());
-            scanny.useDelimiter(",");
-            while(scanny.hasNext()) {
-                list.add(scanny.next());
-                //System.out.println(list);
-                if (list.size() == 7) {
-                	boolean rare = false;
-                    if (list.get(6).equals("true")) {
-                        rare = true;
-                    }
-                    CreatureCard c = new CreatureCard(Integer.parseInt(list.get(1)), list.get(2), Integer.parseInt(list.get(3)),Integer.parseInt(list.get(4)), list.get(5), rare);
-                    hand.put(list.get(0) + c.getCardNum(), c);
-                    //list.clear();
-                }
-                //System.out.println(list);
-            }
-            System.out.println(list);
-            System.out.println(hand);
-        }
-        
-    }
-	public Card removeCard() {
-		return null;
+
+	public Map<String, Card> viewHand() {
+		return hand; //needs toString
 	}
-	
-	
+
+	public Card pickCard() {
+		Random random = new Random();
+		List<String> keys = new ArrayList<String>(deck.keySet());
+		String randomKey = keys.get(random.nextInt(keys.size()));
+		Card value = deck.get(randomKey);
+		removeCard(randomKey);
+		return value;
+	}
+
+	public Card removeCard(String key) {
+		return deck.remove(key);
+	}
 
 }

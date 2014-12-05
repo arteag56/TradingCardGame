36aba15b7b9152e732b19add176f6c8d9952b72c
carbonej4 <Carbonej4@rowan.students.edu>
2014-12-05 03:34:56 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,6 +1,7 @@
 import java.util.*;
 /**
- * [Description]
+ * Class Player allows decks and hands to be created for specific players. 
+ * It also keeps track of that players health. It also keeps track of 
  *
  * @author (Michael Arteaga, Joseph Carbone)
  * @version (Oct 24, 2014)
@@ -9,9 +10,9 @@ public class Player{
 	private String name;
 	private int health = 5;
 	//private boolean completedMove;
-	private Deck deck = new Deck();
-	private List<Card> currentHand = new ArrayList<Card>();
-	private boolean playedMonster=false,playedMagic=false;
+	private Deck deck;
+	private List<Card> currentHand;
+	private boolean playedMonster,playedMagic;
 
 
 	/**
@@ -19,10 +20,17 @@ public class Player{
 	 */
 	public Player() { // default constructor
 		name = "[default]";
-		
+		deck= new Deck(true);
+		playedMonster=false;
+		playedMagic=false;
+		currentHand = new ArrayList<Card>();
 	}
 	public Player(String name) { //constructor
 		this.name = name;
+		deck= new Deck(true);
+		playedMonster=false;
+		playedMagic=false;
+		currentHand = new ArrayList<Card>();
 	}
 	public String getName() {
 		return name;
@@ -55,9 +63,12 @@ public class Player{
 		return health;
 	}
 	public Card draw(){
+		while(currentHand.size()<5){
 		Card result=deck.pickCard();
 		currentHand.add(result);
 		return result;}
+		return currentHand.get(4);}
+		
 	
 	public Card remove(int key){
 		Card result= currentHand.get(key);
@@ -72,12 +83,24 @@ public class Player{
 		playedMonster=false;
 		playedMagic=false;}
 	
-	public boolean getPlayedMonster()
+	public boolean playedMonster()
 	{return playedMonster;}
 	
-	public boolean PlayedMagic()
+	public boolean playedMagic()
 	{return playedMagic;}
 	
+	public static void main(String[] args)
+	{
+		Player p = new Player();
+		p.getDeck();
+		System.out.println("THIS IS THE HAND " + p.getHand());
+		System.out.println("THIS CARD WAS REMOVED "+ p.remove(1).getName());
+		System.out.println("THIS IS NOW THE HAND" + p.getHand());
+		System.out.println("THE NEW CARD IS "+ p.draw().getName());
+		System.out.println("NOW THIS IS THE NEW HAND "+ p.getHand());	
+		System.out.println("PlayedMonster: "+p.playedMonster()+"\n"+ "PlayedMagic: "+p.playedMagic());
+		System.out.println("Completed Turn: "+p.completedMove());
+	}
 
 	
 }
